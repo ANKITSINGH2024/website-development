a2754c7cbc29b4acb749416d8eedf2f20a94a557
ANKIT SINGH <you@example.com>
2021-02-26 17:12:04 +0530

    MINI PROJECT 3

---
+++ student_marks.php
@@ -0,0 +1,17 @@
+<?php
+$name=$_POST['uname'];
+$usn=$_POST['usn'];
+
+$sub1=$_POST['sub1'];
+$sub2=$_POST['sub2'];
+$name=$_POST['sub3'];
+
+$total=$sub1+$sub2+$sub3;
+$avg=$total/3;
+
+print"$usn";
+print"$total";
+
+$print"avg";
+
+?>
